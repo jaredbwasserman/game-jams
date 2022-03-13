d789e91c755a2282368c8ae5bd99a7c716f37dc0
Jared Wasserman <jaredbwasserman@gmail.com>
2022-03-13 15:37:30 -0700

    Update camera

--- bgj-2022-03/scripts/Player.gd
+++ bgj-2022-03/scripts/Player.gd
@@ -1,15 +1,27 @@
 extends KinematicBody
 
-# How fast the player moves in meters per second.
+# How fast the player moves horizontally in meters per second
 export var speed = 14
 
-# The downward acceleration when in the air, in meters per second squared.
+# The downward acceleration when in the air, in meters per second squared
 export var fall_acceleration = 75
 
+# Velocity of player
 var velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
+	# Look at mouse
+	var mouse_intersect = _get_mouse_intersect()
+	if mouse_intersect:
+		var mouse_pos: Vector3 = mouse_intersect.position
+		mouse_pos.y = $Pivot.translation.y
+		$Pivot.look_at(mouse_pos, Vector3.UP)
+
+		# Fix rotation
+		$Pivot.rotation = Vector3(0, $Pivot.rotation.y, 0)
+
+	# Move
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("move_right"):
@@ -23,9 +35,16 @@ func _physics_process(delta):
 
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		$Pivot.look_at(translation + direction, Vector3.UP)
 
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+
+# Get position of mouse in 3D space
+func _get_mouse_intersect():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from = $CameraPivot/Camera.project_ray_origin(mouse_pos)
+	var to = from + $CameraPivot/Camera.project_ray_normal(mouse_pos) * 1000
+	var space_state = get_world().get_direct_space_state()
+	return space_state.intersect_ray(from, to)
